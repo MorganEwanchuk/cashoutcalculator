244c9e95b47a492e524330cca90c10172a8c984f
Morgan <mewanchuk@live.com>
2023-03-31 14:03:26 -0600

    added function for button click

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hi')
+let addButton = document.getElementById('addButton')
+let addName = document.getElementById('addName')
+
+let names = []
+
+addButton.addEventListener('click', () => {
+    names.push(addName.value)
+    console.log(names)
+    addName.value = ""
+})
