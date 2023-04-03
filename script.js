44e1d69326f99d8de71bf6a0aacd2a776c1b334d
Morgan <mewanchuk@live.com>
2023-04-03 12:51:43 -0600

    added function to render names to dom

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let addButton = document.getElementById('addButton')
 let addName = document.getElementById('addName')
+let bartenderNames = document.getElementById('bartenderNames')
 
 let names = {}
 let count = 0
+
 addButton.addEventListener('click', () => {
     
     if(addName.value != ""){
     names[count] = { name: addName.value}
+    addNameToList(addName.value)
     count++
 }else{
     return
@@ -14,3 +17,10 @@ addButton.addEventListener('click', () => {
     console.log(names)
     addName.value = ""
 })
+
+function addNameToList(name){
+    name = document.createElement('p')
+    name.innerText = addName.value
+    bartenderNames.appendChild(name)
+    
+}
